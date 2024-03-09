5cd6b1c958c1c703f63d178bb2f703815424d474
Inigojeevan <inmasm123@gmail.com>
2024-03-09 11:19:49 +0530

    feat: Modal

--- components/Button.tsx
+++ components/Button.tsx
@@ -19,9 +19,7 @@ const Button: React.FC<ButtonProps> = ({
     outline
 }) => {
     return(
-        <div>
-            <button className={`disabled: opacity-70 disabled: cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2 ${fullWidth ? 'w-full' : 'w-fit'} ${secondary ? 'bg-white' : 'bg-sky-500'} ${secondary ? 'text-black' : 'text-white'} ${secondary ? 'border-black' : 'border-sky-500'} ${large ? 'text-xl' : 'text-md'} ${large ? 'px-5' : 'px-4'} ${large ? 'py-3' : 'py-2'} ${outline ? 'bg-transport' : ''} ${outline ? 'border-white' : ''} ${outline ? 'text-white' : ''} `}>{label}</button>
-        </div>
+            <button disabled={disabled} onClick={onClick} className={`disabled: opacity-70 disabled: cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2 ${fullWidth ? 'w-full' : 'w-fit'} ${secondary ? 'bg-white' : 'bg-sky-500'} ${secondary ? 'text-black' : 'text-white'} ${secondary ? 'border-black' : 'border-sky-500'} ${large ? 'text-xl' : 'text-md'} ${large ? 'px-5' : 'px-4'} ${large ? 'py-3' : 'py-2'} ${outline ? 'bg-transport' : ''} ${outline ? 'border-white' : ''} ${outline ? 'text-white' : ''} `}>{label}</button>
     )
 }
 
